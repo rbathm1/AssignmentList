70e9e935b669e45a3e82bd4b66c0fdd37b9f0ae6
Robert <rbathm1@students.towson.edu>
2016-06-08 20:48:24 -0400

    Change DBMS to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160608233534) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "assignments", force: :cascade do |t|
     t.string   "name"
     t.date     "startDate"
