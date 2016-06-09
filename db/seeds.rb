c771757ef5606dd55db5d078fab297c353207d13
Robert <rbathm1@students.towson.edu>
2016-06-08 20:19:35 -0400

    Theme the scaffold with bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Assignment.delete_all
+
+Assignment.create(name: "Lab 0", startDate: "2016-05-23", dueDate: "2016-05-26", complete: "true" )
+Assignment.create(name: "Lab 1", startDate: "2016-05-27", dueDate: "2016-06-01", complete: "true" )
+Assignment.create(name: "Lab 2", startDate: "2016-06-02", dueDate: "2016-06-05", complete: "true" )
+Assignment.create(name: "Lab 3", startDate: "2016-06-06", dueDate: "2016-05-12", complete: "false" )
+Assignment.create(name: "Lab 4", startDate: "2016-06-06", dueDate: "2016-05-12", complete: "false" )
